a798583ac560555ad9fc1d4da51b76be228c94d3
Rahul <rahulwp24@gmail.com>
2025-03-10 14:42:15 +0600

    banner section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Siam from "./Siam";
+import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-red-600 bg-blue-300">hello word</h1>
       <h1 className="text-xl">hello word</h1>
-      <Siam/>
+      <Banner />
+      <Siam />
     </div>
   );
 };
