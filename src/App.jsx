dc90797cff401039bb8ed6cc745f0eebbd360ee6
abdullah107189 <abdullah107189@gmail.com>
2025-03-10 14:17:42 +0600

    set up project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600 bg-blue-300">hello word</h1>
+    </div>
+  );
+};
+
+export default App;
