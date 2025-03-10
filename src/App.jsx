28f724b02413083c4798df69ae27fe73973c1216
abdullah107189 <abdullah107189@gmail.com>
2025-03-10 14:49:31 +0600

    siam vai added compenent

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <h1 className="text-red-600 bg-blue-300">hello word</h1>
       <h1 className="text-xl">hello word</h1>
-      <Siam/>
+      <Siam />
     </div>
   );
 };
