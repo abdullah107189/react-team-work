277f64ae8df402ffe17753a71ae1d7b785669289
siam114 <smsiamislam598@gmail.com>
2025-03-10 14:30:15 +0600

    added brance and name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Siam from "./Siam";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-red-600 bg-blue-300">hello word</h1>
       <h1 className="text-xl">hello word</h1>
+      <Siam/>
     </div>
   );
 };
